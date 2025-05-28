f4058d952c7c6327ce47483c5951d11ecfe4f284
Andrey Potokin <potokin.and@yandex.ru>
2025-05-28 19:54:21 +0300

    Выполнить ФЗ 15-го спринта

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -1,14 +1,17 @@
 package ru.practicum.shareit.booking.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.NewBookingRequest;
 
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface BookingService {
-
+    @Transactional
     BookingDto createBooking(Long userId, NewBookingRequest request);
 
+    @Transactional
     BookingDto updateBookingStatus(Long userId, Long bookingId, Boolean approved);
 
     BookingDto getBooking(Long userId, Long bookingId);
--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.user.dto.NewUserRequest;
 import ru.practicum.shareit.user.dto.UpdateUserRequest;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -7,15 +8,19 @@ import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface UserService {
+    @Transactional
     UserDto createUser(NewUserRequest request);
 
     UserDto getUserById(long userId);
 
     List<UserDto> getUsers();
 
+    @Transactional
     UserDto updateUser(long userId, UpdateUserRequest request);
 
+    @Transactional
     void deleteUser(long userId);
 
     User validateUserExist(Long userId);
